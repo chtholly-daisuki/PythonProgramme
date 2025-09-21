937a197bf48c527dc61b3c64ed96f45bbe3a9fa4
chtholly-daisuki <lhr_0214@163.com>
2025-09-21 13:37:04 +0800

    Update 转pdf_beta.py

--- 转pdf_beta.py
+++ 转pdf_beta.py
@@ -2,7 +2,7 @@ import os
 from fpdf import FPDF
 
 pdf = FPDF()
-pdf.set_auto_page_break(0)         # 自动分页设为False
+pdf.set_auto_page_break(0)         # 自动分页设为Falsetest
 
 path = r"H:\download\图片助手(ImageAssistant)_批量图片下载器\18comic.vip\[まめおじたん]_Knospenmädchen_(中文無修)_H漫內頁瀏覽_Comics_-_禁漫天堂"
 imagelist = [i for i in os.listdir(path)]
