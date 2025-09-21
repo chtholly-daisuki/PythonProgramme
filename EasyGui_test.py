36b248e79b2e539fb226025f5194854a3ba66291
chtholly-daisuki <lhr_0214@163.com>
2025-09-21 13:35:19 +0800

    Update EasyGui_test.py

--- EasyGui_test.py
+++ EasyGui_test.py
@@ -4,7 +4,7 @@ import os
 #easygui.msgbox("嗨，大家好~")
 #string=input("文件该叫什么名字呢？")
 #print(string)
-#pip install PyMuPDF
+#pip install PyMuPDFtyeeee
 #os.mkdir('web版')
 #print("Ciallo～(∠・ω< )⌒☆很高兴和你见面，先让我明确您的要求\n您要保留webp图片吗")
 for i in os.listdir(os.getcwd()):
