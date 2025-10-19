63b35c3f555712d402364c5d980f655d50b92a3c
chtholly-daisuki <lhr_0214@163.com>
2025-10-20 01:20:31 +0800

    清除不必要文件
    清除不必要文件

--- wbeb转pdf.py
+++ wbeb转pdf.py
@@ -5,7 +5,7 @@ doc = fitz.open()
 # H:\download\图片助手(ImageAssistant)_批量图片下载器\18comic.vip\[まめおじたん]_Knospenmädchen_(中文無修)_H漫內頁瀏覽_Comics_-_禁漫天堂
 # H:/download/图片助手(ImageAssistant)_批量图片下载器/18comic.vip/[まめおじたん]_Knospenmädchen_(中文無修)_H漫內頁瀏覽_Comics_-_禁漫天堂
 # 循环path中的文件，可import os 然后用 for img in os.listdir(img_path)实现
-# 这里为了让文件以1，2，3的形式进行拼接，就偷懒循环文件名中的数字。
+# 这里为了让文件以1，2，3的形式进行拼接，就偷懒循环文件名中的数字。test
 for i in range(10001,10178):
 	img = str(i) + '.jpg'
 	img_file = img_path + img
